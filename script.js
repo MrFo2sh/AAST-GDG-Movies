ca0e06eff022be87e934e69e5d5cad33400bd401
Mohamed Fouad <m.fouad2912@gmail.com>
2024-12-08 00:00:36 +0200

    bootstrap

--- script.js
+++ script.js
@@ -1,36 +1,28 @@
-const INTERVAL = 3000;
-
-const SLIDER = document.getElementById("slider");
-const SLIDER_TITLE = document.getElementById("slider-title");
-const SLIDER_DESCRIPTION = document.getElementById("slider-description");
-
 const MOVIE_LIST = document.getElementById("movie-list");
+const INTERVAL = 4000;
+const swiperEl = document.querySelector("swiper-container");
 
-var slideIndex = 0;
+const swiper = new Swiper(".swiper", {
+  // Default parameters
+  effect: "fade",
+  depth: 100,
+  modifier: 1,
+  rotate: 50,
+  scale: 1,
+  loop: true,
+  stretch: 0,
+  slideShadows: true,
+  // spaceBetween: 10,
+  // Responsive breakpoints
+});
 
-const SLIDES = [
-  {
-    url: "wp7946869.jpg",
-    title: "Fast & Furious",
-    description:
-      "When a crime brings them back to L.A., fugitive ex-con Dom Toretto reignites his feud with agent Brian O'Conner. But as they are forced to confront a shared enemy, Dom and Brian must give in to an uncertain new trust if they hope to outmaneuver him. And the two men will find the best way to get revenge: push the limits of what's possible behind the wheel.",
-  },
-  {
-    url: "24.jpg.webp",
-    title: "Dark Knight",
-    description:
-      "Batman raises the stakes in his war on crime. With the help of Lt. Jim Gordon and District Attorney Harvey Dent, Batman sets out to dismantle the remaining criminal organizations that plague the streets. The partnership proves to be effective, but they soon find themselves prey to a reign of chaos unleashed by a rising criminal mastermind known to the terrified citizens of Gotham as the Joker.",
-  },
-  {
-    url: "tom_holland_spider_man_hd_spider_man_no_way_home-2560x1440.jpg",
-    title: "Spider Man",
-    description:
-      "After being bitten by a genetically altered spider, nerdy high school student Peter Parker is endowed with amazing powers.",
-  },
-];
+setInterval(() => {
+  swiper.slideNext();
+}, INTERVAL);
 
 const MOVIES = [
   {
+    id: 1,
     url: "p_moana2_payoff_5787a994.jpeg",
     title: "Moana 2",
     date: "2024",
@@ -38,6 +30,7 @@ const MOVIES = [
   },
 
   {
+    id: 2,
     url: "venom.jpeg",
     title: "Venom",
     date: "2024",
@@ -45,6 +38,7 @@ const MOVIES = [
   },
 
   {
+    id: 3,
     url: "p_moana2_payoff_5787a994.jpeg",
     title: "Moana 2",
     date: "2024",
@@ -52,26 +46,13 @@ const MOVIES = [
   },
 
   {
+    id: 4,
     url: "venom.jpeg",
     title: "Venom",
     date: "2024",
     duration: 120,
   },
 ];
-
-updateSlide();
-
-setInterval(() => {
-  updateSlide();
-}, INTERVAL);
-
-function updateSlide() {
-  SLIDER_TITLE.innerText = SLIDES[slideIndex].title;
-  SLIDER_DESCRIPTION.innerText = SLIDES[slideIndex].description;
-  SLIDER.style.backgroundImage = `url("assets/${SLIDES[slideIndex].url}")`;
-  slideIndex++;
-  if (slideIndex === 3) slideIndex = 0;
-}
 
 function displayMovies() {
   for (let movie of MOVIES) displayMovie(movie);
@@ -79,7 +60,7 @@ function displayMovies() {
 
 function displayMovie(movie) {
   const card = `
-    <div class="movie-card">
+    <div class="movie-card" onclick="openMovie(${movie.id})">
         <img src="/movies/${movie.url}" alt="" />
         <p>${movie.title}</p>
         <div class="movie-details">
@@ -90,6 +71,10 @@ function displayMovie(movie) {
     `;
 
   MOVIE_LIST.innerHTML += card;
+}
+
+function openMovie(id) {
+  window.location.href = "movie.html?id=" + id;
 }
 
 displayMovies();
